9025a0944e4d5427af19e9c5e6dd28d7c0935dd5
Tarsila <tarsilabfritz0305@outlook.com>
2024-04-08 15:54:21 -0300

    update: clear all button

--- script.js
+++ script.js
@@ -2,17 +2,35 @@
 const addButton = document.getElementById('add-task-button');
 const taskTextarea = document.getElementById('task-name');
 const tasksContainer = document.getElementById('tasks-container')
+const clearButton = document.getElementById('clear-tasks')
 
-// Add button function
-addButton.addEventListener('click', function(){
+// Function add task
+function addTask(){
     const taskText = taskTextarea.value.trim();
 
-   if (taskText !== ''){
+    if (taskText !== ''){
     const taskDiv = document.createElement('div');
     taskDiv.className = 'task';
     taskDiv.textContent = taskText;
     tasksContainer.appendChild(taskDiv);
     taskTextarea.value = '';
-   } 
+    }
 }
-)
+
+// Button function
+addButton.addEventListener('click', addTask);
+
+// Enter function
+taskTextarea.addEventListener('keypress', function(event){
+    if (event.key === "Enter"){
+        event.preventDefault(); // Prevent the "Enter" key from causing a line break in the textarea
+        addTask();  // Call the function to add the task
+    }
+})
+
+// Clear All function
+function clearContainer(){
+    tasksContainer.innerHTML = '';
+}
+
+clearButton.addEventListener('click', clearContainer)
